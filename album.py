0a40ab3617195199de42e1a2fb7de0f4697f46b2
LukeZavierWieder <lzw5614@psu.edu>
2026-03-21 10:44:49 -0400

    Add: Code for Album Class

--- album.py
+++ album.py
@@ -8,3 +8,55 @@ Key concepts to implement:
   • A *derived* property (debut_year) that computes its value from stored data
     rather than keeping a second field in sync.
 """
+
+class Album:
+    '''
+    Creates the Album class.
+    '''
+    
+    # Constructer
+    def __init__(self, title : str, active : bool, years : list[int]):
+        # Error for empty list
+        if not years:
+            raise ValueError("Years list must not be empty.")
+        # Name of album
+        self._title = title
+        # Is the album active 
+        self._active = active
+        # Years the album was released
+        self.years = list(years)
+
+    # Properties (getters)
+    @property
+    def title(self):
+        '''
+        returns the title
+        
+        Parameter:
+            self: 
+        '''
+        return self._title
+    @property
+    def active(self):
+        '''
+        returns the activity
+        
+        Parameter:
+            self: 
+        '''
+        return self._active
+    @property
+    def years(self):
+        '''
+        returns the activity
+        
+        Parameter:
+            self: 
+        '''
+        return list(self._years)
+    
+    def __str__(self):
+        '''
+        Prints out the Artist object
+        '''
+        return f"{self._title}. active = {self._active}, debut year: {self._years[0]}"
