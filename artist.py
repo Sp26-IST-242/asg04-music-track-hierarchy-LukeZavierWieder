2e174294161674fcb5c5e34322374705889db351
LukeZavierWieder <lzw5614@psu.edu>
2026-03-21 10:35:06 -0400

    Add: Code for Artist Class

--- artist.py
+++ artist.py
@@ -6,3 +6,41 @@ It introduces two core Python OOP conventions:
   1. The single leading-underscore (_name) signals a non-public attribute.
   2. @property exposes a clean public getter without allowing direct mutation.
 """
+
+class Artist:
+    '''
+    Creates the Artist class.
+    '''
+    
+    # Constructer
+    def __init__(self, name : str, genre : str):
+        # Name of artist
+        self._name = name
+        # Genre of the artist
+        self._genre = genre
+
+    # Properties (getters)
+    @property
+    def name(self):
+        '''
+        returns the name
+        
+        Parameter:
+            self: 
+        '''
+        return self._name
+    @property
+    def genre(self):
+        '''
+        returns the genre
+        
+        Parameter:
+            self: 
+        '''
+        return self._genre
+    
+    def __str__(self):
+        '''
+        Prints out the Artist object
+        '''
+        return f"{self._name}, {self._genre}"
