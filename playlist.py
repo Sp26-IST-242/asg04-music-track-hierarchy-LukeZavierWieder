6b11a219e37fec5f198947e01d7cd23d6313bcbf
LukeZavierWieder <lzw5614@psu.edu>
2026-03-21 22:46:45 -0400

    Add: Code for Playlist Class

--- playlist.py
+++ playlist.py
@@ -11,3 +11,31 @@ Design notes:
   • __str__ uses a generator expression with str.join() for a concise
     multi-line string without building an intermediate list manually.
 """
+class Playlist:
+    '''
+    Creates the Playlist Class
+    '''
+    # Constructor
+    def __init__(self, tracks : list[str]):
+        # The list of tracks
+        self._tracks = []
+    # Getter
+    @property
+    def tracks(self):
+        return self._tracks.copy()
+    
+    def add_track(self, track):
+        # Adds a track to the list
+        self._tracks.append(track)
+
+    def clear_playlist(self):
+        # Clears the track list
+        self._tracks.clear() 
+
+    def sort_by_release_year(self):
+        # Sorts the tracks by their release year
+        self._tracks.sort()
+
+    def __str__(self):
+        # Prints the playlist
+        return "\n".join(str(track) for track in self._tracks)
