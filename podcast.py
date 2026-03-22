3818396526435139498ef233a44e4b7f9d9a73d4
LukeZavierWieder <lzw5614@psu.edu>
2026-03-21 22:22:58 -0400

    Add: Code for Podcast Class

--- podcast.py
+++ podcast.py
@@ -23,3 +23,38 @@ Podcast differs from Song in two ways:
             (Joe Rogan, Comedy) The Joe Rogan Experience active = True,
              debut year: 2009, duration: 02:30:00 is explicit: True
 """
+from music_track import MusicTrack
+from artist import Artist
+from album import Album
+
+class Podcast(MusicTrack):
+   # constructor
+   def __init__(
+         self, artist: Artist, album: Album,
+         duration_seconds: int, is_explicit: bool = False):
+      super().__init__(artist, album, duration_seconds)
+      # Set If explicit
+      self._is_explicit = is_explicit
+
+   # Getters
+   @property
+   def is_explicit(self):
+      # Returns if explicit
+      return self._is_explicit
+   @property
+   def release_year(self):
+       return self.album.debut_year
+   @property
+   def duration_seconds(self):
+       return self._duration_seconds
+
+   # Format playtime
+   def play_time_formatted(self):
+      time = int(self._duration_seconds)
+      hours = time // 3600
+      minutes = (time % 3600) // 60
+      seconds = time % 60
+      return f"{hours:02}:{minutes:02}:{seconds:02}"
+   
+   def __str__(self):
+      return f'({self._artist}) {self._album} duration: {self.play_time_formatted()} is explicit: {self._is_explicit}'
